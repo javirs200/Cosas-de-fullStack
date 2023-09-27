69085838e988b11e9499c8b15f051d9c93b4b6dc
rsmax <fordrsmax@gmail.com>
2023-09-27 12:32:54 +0200

    add js beahvour to template

---
+++ web template/scripts/script.js
@@ -0,0 +1,22 @@
+//variables globales
+
+
+//--------------esta es la funcion del enuciado , lo demas es adorno XD -----------------*/
+
+
+
+//---------------------------------------------------------------------------*/
+
+
+window.onload = () => {
+    let boton = document.getElementById("accion");
+  
+    if (boton != null) {
+  
+      boton.onclick = () => {
+  
+      }
+    }
+  }
+  
+  
