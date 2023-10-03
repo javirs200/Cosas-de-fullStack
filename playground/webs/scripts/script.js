4a8ebe1aa36b07f9968226f04069bb995679e494
javirs200 <javier.miranda.tejada@alumnos.upm.es>
2023-10-03 13:33:07 +0200

    update playground

--- playground/webs/scripts/script.js
+++ playground/webs/scripts/script.js
@@ -1,47 +1,34 @@
-/***1 -** Hacer un boton que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible
-
-**2 -** Hacer que con onmouseover y onmouseout, se muestre/esconda el título de una foto cuando pases el raton sobre dicha foto
-
-**3 -** Añadir dos elementos `<li>` a un `<ul>`, y unirlos al DOM de tu página HTML
-	Usar:
-	- innerHTML
-	- appendChild()
-	- createElement()
-	- createAttribute()
-    	- createTextNode()*/
-
-
-		
 window.onload = () => {
 
-	function mostrarOcultar(){
-		//let parrrafo = document.getElementById("textParagraph")
-		let parrrafo = document.querySelector("p#textParagraph")
-		parrrafo.hidden = !parrrafo.hidden
+	let miform = document.getElementById("miform")
+
+	miform.addEventListener("submit", (event) => {
+
+		//strings
+
+		let enviado = "Has enviado formulario!!!"
+
+		let nameLengthWarn = "error en la longitud del nombre"
+
+		event.preventDefault(); // parar envío formulario
+
+		console.log(enviado);
+
+		//console.log(event); // objeto con datos
+		//console.log(event.target.elements); // inputs del form
+
+		let data = event.target
+
+		let fname = data.name.value
+
+		console.log("nombre", fname);
+
+		if (fname.length < 3 || fname.length > 5) {
+			console.log(nameLengthWarn)
+			//alert(nameLengthWarn)
+		}
+
 	}
-
-	let boton = document.getElementById("toggleButton")
-
-	boton.addEventListener("click",mostrarOcultar)
-
-	function spawn(){
-		let main = document.querySelector("main")
-		let lista = document.createElement("ul")
-		let li1 = document.createElement("li")
-		let art = document.createAttribute("cosa")
-		li1.id = art
-		li1.innerHTML="patatas"
-		let li2 = document.createElement("li")
-		let node = document.createTextNode("nodo")
-		lista.appendChild(li1)
-		li2.appendChild(node)
-		lista.appendChild(li2)
-		main.appendChild(lista)
-
-	}
-
-	let boton2 = document.getElementById("crear")
-
-	boton2.addEventListener("click",spawn)
+	)
 
 }
