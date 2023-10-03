e9257b1017def154492cce0b234465299a968fc3
javirs200 <javier.miranda.tejada@alumnos.upm.es>
2023-10-03 09:46:38 +0200

    katas mañaneras

--- playground/scripts/katas/script.js
+++ playground/scripts/katas/script.js
@@ -279,6 +279,28 @@ function isNarcissistic2(n) {
   return result == n
   }
 
+  /*There are pillars near the road. The distance between the pillars 
+  is the same and the width of the pillars is the same. 
+  Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters
+ (without the width of the first and last pillar).*/
+
+ function pillars(numPill, dist, width) {
+  
+    if(numPill > 1) {
+      //huecos entre postes
+      let inter = (numPill-1)*dist*100
+      //grosores de los postes intermedios 
+      let grosores = (numPill-2)*width
+        return grosores + inter;
+    } else {
+      return 0;
+    }
+  }
 
 window.onload = () => {
     let boton = document.getElementById("accion");
@@ -286,7 +308,7 @@ window.onload = () => {
     if (boton != null) {
 
         boton.onclick = () => {
-            isNarcissistic(153)
+            console.log(pillars(2, 20, 25) , 2000);
         }
     }
 }
