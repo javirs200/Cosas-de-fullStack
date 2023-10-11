3af46efe7b8d3ddfcca6a9d067e99c7795e901e6
javirs200 <fordrsmax@gmail.com>
2023-10-11 12:09:50 +0200

    update playgorund/localjs

--- playground/localjs/scripts/script.js
+++ playground/localjs/scripts/script.js
@@ -1,7 +1,26 @@
 //playground for local js
 
-function local() {
+async function local() {
     console.log("local");
+    let str = "hola"
+    let keyPair = await crypto.subtle.generateKey(
+        {
+          name: "RSA-OAEP",
+          modulusLength: 4096,
+          publicExponent: new Uint8Array([1, 0, 1]),
+          hash: "SHA-256",
+        },
+        true,
+        ["encrypt", "decrypt"],
+      );
+    let encripted = await crypto.subtle.encrypt(
+        {
+          name: "RSA-OAEP",
+        },
+        keyPair[0],
+        str,
+      );
+    console.log(encripted);
 }
 
 window.onload = () => {
